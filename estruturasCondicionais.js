3ab13ad051570e81bf08a185698649ff02e5faa5
Diego-Teixeira-dev <diegoteixeira2000@gmail.com>
2025-02-06 13:34:47 -0300

    Verificando um valor usando estruturas condicionais.

---
+++ estruturasCondicionais.js
@@ -0,0 +1,10 @@
+// Operação com estruturas condicionais.
+
+let nota1 = 8;
+let nota2 = 3;
+let media = (nota1 + nota2) / 2;
+if (media > 6) {
+    console.log("Aprovado: ");
+} else {
+    console.log("Reprovado: ");
+}
